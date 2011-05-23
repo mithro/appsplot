b686f9ea412224027aa4103d1f97237d9f057b18
Tim 'mithro' Ansell <mithro@mithis.com>
2011-05-23 13:49:25 +1000

    Adding Google analytics.

--- index.php
+++ index.php
@@ -54,6 +54,22 @@ header("Content-Type: text/html; charset=UTF-8");
   <style type='text/css'>
 <?php include 'index.min.css'; ?>
   </style>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22631058-1']);
+  _gaq.push(['_setDomainName', '.appsplot.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 </head>  
 
 <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js' type='text/javascript' charset='utf-8'></script>
